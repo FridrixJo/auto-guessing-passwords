72bc5b6786e01cacfbc39c91f8076eb2bdea2556
kells <pashaglytov2004@mail.ru>
2022-11-19 19:21:03 +0300

    browser implementation

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 import asyncio
 
+from aiogram import types
+
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 
 import config
+
+from asyncio_browser import WebBrowser
 
 storage = MemoryStorage()
 
@@ -20,6 +24,33 @@ bot = Bot(token=config.TOKEN)
 dispatcher = Dispatcher(bot=bot, storage=storage)
 
 ADMIN_IDS = [int(config.ADMIN_ID)]
+
+
+@dispatcher.message_handler(commands=['start'])
+async def get_started(message: types.Message):
+    br = WebBrowser()
+    param = await br.initialize()
+    await asyncio.sleep(10)
+    if param is True:
+        new_param = await br.get_page()
+        if new_param is True:
+            while True:
+                a = await br.input_login()
+                b = await br.input_password()
+
+                if a and b is True:
+                    data = await br.press_btc()
+                    if data[0] is True:
+                        print('find')
+                    else:
+                        if data[1] == 'not':
+                            print('not find')
+                        else:
+                            print('error')
+        else:
+            print('cannot get a page')
+    else:
+        print('not initialized')
 
 
 try:
